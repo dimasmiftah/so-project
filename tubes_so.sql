47470800699b421b7b97383f6d784c1903c81ed1
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2020-07-03 23:40:00 +0700

    alter tabel transaksi and list_transaksi

--- tubes_so.sql
+++ tubes_so.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 27, 2020 at 02:42 PM
--- Server version: 10.4.8-MariaDB
--- PHP Version: 7.3.11
+-- Generation Time: Jul 03, 2020 at 06:38 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -35,6 +35,17 @@ CREATE TABLE `barang` (
   `harga` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id_barang`, `barang`, `stok`, `harga`) VALUES
+(1, 'Laptop', 5, 8000000),
+(2, 'Keyboard', 5, 2000000),
+(3, 'JBL', 0, 300000),
+(4, 'Buku', 0, 2000),
+(5, 'Pensil', 7, 1000);
+
 -- --------------------------------------------------------
 
 --
@@ -43,11 +54,22 @@ CREATE TABLE `barang` (
 
 CREATE TABLE `list_transaksi` (
   `id_list_transaksi` int(11) NOT NULL,
-  `id_transaksi` int(11) NOT NULL,
+  `id_transaksi` varchar(13) NOT NULL,
   `id_barang` int(11) NOT NULL,
   `qty` int(11) NOT NULL,
   `total` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `list_transaksi`
+--
+
+INSERT INTO `list_transaksi` (`id_list_transaksi`, `id_transaksi`, `id_barang`, `qty`, `total`) VALUES
+(1, '1593787652636', 1, 4, 32000000),
+(2, '1593787652636', 2, 2, 4000000),
+(3, '1593793111169', 2, 2, 4000000),
+(4, '1593793737865', 4, 2, 4000),
+(5, '1593793737865', 2, 5, 10000000);
 
 -- --------------------------------------------------------
 
@@ -56,11 +78,20 @@ CREATE TABLE `list_transaksi` (
 --
 
 CREATE TABLE `transaksi` (
-  `id_transaksi` int(11) NOT NULL,
+  `id_transaksi` varchar(13) NOT NULL,
   `id_user` varchar(10) NOT NULL,
   `tanggal` date NOT NULL,
   `subtotal` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `id_user`, `tanggal`, `subtotal`) VALUES
+('1593787652636', 'USER-000', '2020-07-03', 36000000),
+('1593793111169', 'USER-000', '2020-07-03', 4000000),
+('1593793737865', 'USER-000', '2020-07-03', 10004000);
 
 -- --------------------------------------------------------
 
@@ -124,19 +155,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `list_transaksi`
 --
 ALTER TABLE `list_transaksi`
-  MODIFY `id_list_transaksi` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `transaksi`
---
-ALTER TABLE `transaksi`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_list_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
@@ -146,8 +171,8 @@ ALTER TABLE `transaksi`
 -- Constraints for table `list_transaksi`
 --
 ALTER TABLE `list_transaksi`
-  ADD CONSTRAINT `list_transaksi_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`),
-  ADD CONSTRAINT `list_transaksi_ibfk_2` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`);
+  ADD CONSTRAINT `list_transaksi_ibfk_2` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`),
+  ADD CONSTRAINT `list_transaksi_ibfk_3` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`);
 
 --
 -- Constraints for table `transaksi`
