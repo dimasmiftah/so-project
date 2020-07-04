dad8906392071005b7d8d2c531adedb8995dd882
TaufiqRizky <taufiq.it2@gmail.com>
2020-07-04 17:47:33 +0700

    fix koneksi

--- Auth/koneksi.php
+++ Auth/koneksi.php
@@ -6,20 +6,6 @@ if (mysqli_connect_errno()){
 	echo "Koneksi database gagal : " . mysqli_connect_error();
 }
 
-if(isset($_SESSION['role']=="admin")){
- 
-		
-		header("location:../Dashboard/halaman_admin.php");
- 
-	// cek jika user login sebagai kasir
-	}else if(isset($_SESSION['role']=="kasir")){
-		
-		header("location:../Dashboard/halaman_kasir.php");
- 
-	}else{
- 
-		// alihkan ke halaman login kembali
-		header("location:../index.php?pesan=gagal");
-	}	
+
 
 ?>
